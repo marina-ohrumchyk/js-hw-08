394e311da1ef7ef73cdb506f298ba40721cd5f14
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-01-26 23:17:45 +0200

    src start commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const cardMarkup = createCard(galleryItems);
+galleryConteiner.insertAdjacentHTML('beforeend', cardMarkup);
+galleryConteiner.addEventListener('click', galleryClick);
+
+function createCard(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    })
+    .join('');
+}
+
+function galleryClick(evt) {
+  evt.preventDefault();
+  
+  if (evt.target.nodeName !== `IMG`) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600">
+      `);
+  instance.show();
+}
