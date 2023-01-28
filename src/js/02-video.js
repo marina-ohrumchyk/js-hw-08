712af2eb00beffcfaba8d90d334a99e0a9d02539
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-01-29 00:26:52 +0200

    video commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+// import Player from "@vimeo/player"
+// import throttle from "lodash.throttle"
 
+// const iframe = document.querySelector('iframe');
+//     const player = newPlayer(iframe);
+
+//     const onPlay = ({ seconds }) => {
+//         localStorage.setItem ("videoplayer-current-time", seconds);
+
+//     };
+
+//     player.on("timeupdate", throttle(onPlay, 1000));
+// player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
