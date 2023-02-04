5a4f02a9c807bc940f6b6781e48fca6d955637f2
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-02-04 23:55:20 +0200

    throttle commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,20 @@
-// import Player from "@vimeo/player"
+import Player from "@vimeo/player"
 // import throttle from "lodash.throttle"
 
-// const iframe = document.querySelector('iframe');
-//     const player = newPlayer(iframe);
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
 
-//     const onPlay = ({ seconds }) => {
-//         localStorage.setItem ("videoplayer-current-time", seconds);
+    const onPlay = ({ seconds }) => {
+        localStorage.setItem ("videoplayer-current-time", seconds);
 
-//     };
+    };
 
-//     player.on("timeupdate", throttle(onPlay, 1000));
-// player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    player.on("timeupdate", throttle(onPlay, 1000));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+
+// player.on("timeupdate", throttle(function (data) {
+//     const time = data.seconds;
+//     console.log(time);
+//    localStorage.setItem(TIME_DATA_PLAYER, time)
+// },1000))
