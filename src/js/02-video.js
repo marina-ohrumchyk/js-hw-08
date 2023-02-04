6a5fc548b0aef82b6680f2a5bc449a4bb1651c55
Marina <111200416+marina-ohrumchyk@users.noreply.github.com>
2023-02-05 00:59:21 +0200

    finish commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,15 @@
-import Player from "@vimeo/player"
-// import throttle from "lodash.throttle"
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
-    const player = new Player(iframe);
+const player = new Player(iframe);
 
-    const onPlay = ({ seconds }) => {
-        localStorage.setItem ("videoplayer-current-time", seconds);
-
-    };
-
-    player.on("timeupdate", throttle(onPlay, 1000));
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
-
-
-// player.on("timeupdate", throttle(function (data) {
-//     const time = data.seconds;
-//     console.log(time);
-//    localStorage.setItem(TIME_DATA_PLAYER, time)
-// },1000))
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const time = data.seconds;
+    console.log(time);
+    localStorage.setItem('videoplayer-current-time', time);
+  }, 1000)
+);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formElement = document.querySelector('.feedback-form');
+const inputElement = document.querySelector('input');
+const textareaElement = document.querySelector('textarea');
+
+formElement.addEventListener('submit', submitForm);
+inputElement.addEventListener('input', throttle(addlocalStorage, 500));
+
+const localObject = {
+  email: '',
+  message: '',
+};
+
+function submitForm(evt) {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function addlocalStorage(evt) {
+  localObject[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(localObject));
+}
+
+const dataObject = localStorage.getItem('feedback-form-state');
+function insertWithLS(object) {
+  if (object && inputElement.name === 'email') {
+    inputElement.value = JSON.parse(object).email;
+  }
+
+  if (object && textareaElement.name === 'message') {
+    textareaElement.value = JSON.parse(object).message;
+  }
+}
+insertWithLS(dataObject);
